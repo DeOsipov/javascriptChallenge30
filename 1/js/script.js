d5a4978a0d6f219ea885e6635b6cff59f79c3ba0
Denis Osipov <whomi@mail.ru>
2018-12-04 21:51:23 +0300

    initial

---
+++ 1/js/script.js
@@ -0,0 +1,18 @@
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    /*if (!audio) return;*/
+
+    key.classList.add('activ');
+    audio.currentTime = 0;
+    audio.play();
+}
+
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('activ');
+}
+
+const keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
